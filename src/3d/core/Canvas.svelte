57148cd304d48c3356aa89091b85511013fa8071
jrunge <jrunge@precisionplanting.com>
2024-02-14 15:21:15 -0600

    Optimizing assets bringing scene from 82 to 30MB

--- src/3d/core/Canvas.svelte
+++ src/3d/core/Canvas.svelte
@@ -22,40 +22,17 @@
 
         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
         camera.minZ = 0.1;
-        var table: Mesh = MeshBuilder.CreateBox("table", { width: 63, height:23, depth: .2 }, scene);
-        table.position.z = -15;
-        let tableMaterial = new PBRMaterial("tableMaterial", scene);
-
-        let woodTexturePack = {
-            albedo: new Texture("/assets/tex/WoodFineDark003/WoodFineDark003_COL_2K.jpg", scene),
-            emissive: new Texture("/assets/tex/WoodFineDark003/WoodFineDark003_GLOSS_2K.jpg", scene),
-            bump: new Texture("/assets/tex/WoodFineDark003/WoodFineDark003_NRM_2K.jpg", scene),
-            reflectance: new Texture("/assets/tex/WoodFineDark003/WoodFineDark003_REFL_2K.jpg", scene),
-            displacement: new Texture("/assets/tex/WoodFineDark003/WoodFineDark003_DISP_2K.jpg", scene)
-        }
-
-        for(let t in woodTexturePack) {
-            woodTexturePack[t as keyof typeof woodTexturePack].uScale = 5;
-            woodTexturePack[t as keyof typeof woodTexturePack].vScale = 3;
-        }
-
-        tableMaterial.albedoTexture = woodTexturePack.albedo;
-        tableMaterial.emissiveTexture = woodTexturePack.emissive;
-        tableMaterial.bumpTexture = woodTexturePack.bump;
-        tableMaterial.reflectanceTexture = woodTexturePack.reflectance;
-        tableMaterial.metallic = 0;
-        tableMaterial.roughness = 0.1;
-        table.material = tableMaterial;
 
         // SceneLoader.ImportMesh("", "/assets/model/dell_xps_13_plus_closed/", "scene.gltf");
-        SceneLoader.Append("/assets/model/apple_ipad_pro/", "scene.glb", scene, (scene) => {
+        SceneLoader.Append("/assets/model/", "desktop.glb", scene, (scene) => {
             console.log(scene);
             console.log(scene.getMeshById("__root__"));
-            if(scene.getMeshById("__root__") !== null) scene.getMeshById("__root__").position = new Vector3(0, 0, 0);
+            // if(scene.getMeshById("__root__") !== null) scene.getMeshById("__root__").position = new Vector3(0, 0, 0);
         });
 
-        camera.attachControl(canvas, true);
+        // camera.attachControl(canvas, true);
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        light1.intensity = 0.5;
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
@@ -85,5 +62,6 @@ onMount(()=> new App());
     canvas {
         width: 100%;
         height: 100%;
+        z-index: 0;
     }
 </style>
