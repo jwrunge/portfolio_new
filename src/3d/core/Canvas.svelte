6dfb42a44ccc385dda40fca3cc8702236b98e78d
jrunge <jrunge@precisionplanting.com>
2024-02-14 22:13:47 -0600

    Pictures and desktop screen

--- src/3d/core/Canvas.svelte
+++ src/3d/core/Canvas.svelte
@@ -20,18 +20,20 @@
 
         scene.clearColor = new Color4(0,0,0,0);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new Vector3(0, 1, 0), scene);
+        camera.target = new Vector3(0, 0, 0);
         camera.minZ = 0.1;
+        camera.attachControl(canvas, true);
 
         // SceneLoader.ImportMesh("", "/assets/model/dell_xps_13_plus_closed/", "scene.gltf");
         SceneLoader.Append("/assets/model/", "desktop.glb", scene, (scene) => {
-            console.log(scene);
-            console.log(scene.getMeshById("__root__"));
-            // if(scene.getMeshById("__root__") !== null) scene.getMeshById("__root__").position = new Vector3(0, 0, 0);
+            const root = scene.getMeshById("__root__");
+            if(root) root.position = new Vector3(0, 0, 0);
+            root?.scaling.scaleInPlace(2);
         });
 
         // camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(5, 5, 0), scene);
         light1.intensity = 0.5;
 
         // hide/show the Inspector
@@ -62,6 +64,5 @@ onMount(()=> new App());
     canvas {
         width: 100%;
         height: 100%;
-        z-index: 0;
     }
 </style>
